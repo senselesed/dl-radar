bdfb02517e3a434e5aa45596b39f8697a6d3fd2d
wav <165479057+senselesed@users.noreply.github.com>
2026-05-09 18:34:21 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const { initializeApp } = require("firebase/app");
+const { getDatabase, ref, set } = require("firebase/database");
+
+const firebaseConfig = {
+    databaseURL: "https://urlink.firebaseio.com"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+set(ref(db, 'radar'), {
+    sent_at: Math.floor(Date.now() / 1000),
+    local: { id: 3, x: 0, y: 0 },
+    allies: [0],
+    enemies: [0]
+}).then(() => {
+    console.log("Структура 'radar' успешно создана!");
+    process.exit(); // Завершаем работу скрипта
+});
